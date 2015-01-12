879d5a907f134b1c72b349c661d63b64fe187bcb
ania macioszek <a.macioszek@students.mimuw.edu.pl>
2015-01-12 13:11:35 +0100

    class EpickiKonwerter; list of attributes

---
+++ EpickiKonwerter.py
@@ -0,0 +1,16 @@
+class EpickiKonwerter:
+
+	def __init__( self ):
+		self.sequence = []
+		self.reference = []
+		self.source = []
+		self.method = []
+		self.start = []
+		self.stop = []
+		self.score = []
+		self.strand = []
+		self.phase = []
+		self.group = []	# lista slownikow
+
+		
+
