8ac6ac90c9d0aca2daa16dbf2d1c973fc9465222
ania macioszek <a.macioszek@students.mimuw.edu.pl>
2015-01-15 21:27:45 +0100

    readFASTA() method additional attribute 'descriptions' changed attribute's names to plural

--- EpickiKonwerter.py
+++ EpickiKonwerter.py
@@ -1,16 +1,29 @@
 class EpickiKonwerter:
 
 	def __init__( self ):
-		self.sequence = []
-		self.reference = []
-		self.source = []
-		self.method = []
-		self.start = []
-		self.stop = []
-		self.score = []
-		self.strand = []
-		self.phase = []
-		self.group = []	# lista slownikow
+		self.sequences = []
+		self.references = []
+		self.sources = []
+		self.methods = []
+		self.starts = []
+		self.stops = []
+		self.scores = []
+		self.strands = []
+		self.phases = []
+		self.groups = []	# lista slownikow
+		self.descriptions = []
 
-		
+	def readFASTA( self, filename ):
+		lines = open( filename ).readlines()
+		for line in lines:
+			if line.startswith('>'):
+				self.descriptions.append( line[1:].strip() )
+				self.sequences.append('')
+			else:
+				self.sequences[-1] += line.strip()
+
+
+
+	def writeFASTA( self, filename ):
+		return 0
 
