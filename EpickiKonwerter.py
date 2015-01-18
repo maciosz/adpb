a2532be5c347d841769bd1c649bfdb35303ee310
Marcin Świstak <m.swistak@students.mimuw.edu.pl>
2015-01-18 18:53:28 +0100

    Write SAM

--- EpickiKonwerter.py
+++ EpickiKonwerter.py
@@ -1,10 +1,11 @@
 import re
+import csv
 
 class EpickiKonwerter:
 
 	def __init__( self ):
 		self.sequences = []
-		self.SAMsequences = {}	#list of pairs (seqID, seq)
+		self.seqIDs = []
 		self.references = []
 		self.refURIs = {}	#dictionary {refID: URI}
 		self.refLens = {}	#dictionary {refID: length}
@@ -64,15 +65,50 @@ class EpickiKonwerter:
 					#additional comments
 
 			else:
-				self.sequences.append(line[9])
+				self.seqIDs.append(line[0])
 				self.references.append(line[2])
 				self.starts.append(line[3])
 				self.ends.append(max([sum([int(i) for i in re.findall('\d+', c)]+[0]), len(line[9])]))
 				self.MAPQ.append(line[4])
 				self.CIGAR.append(line[5])
+				self.sequences.append(line[9])
 				self.quality.append(line[10])
 				self.descriptions.append(line[11])
 
 	def writeSAM( self, filename ):
 	"""Method for writing sequence data in SAM format."""
+		with open( filename, 'w' ) as out:
+			writer = csv.writer(out, delimiter='\t')
 
+			writer.writerow(['@HD\tVN:1.0\tSO:unknown'])
+
+			refIDs = set(self.refLens.keys()).union( \
+				 set(self.refURIs.keys())
+			for refID in refIDs:
+				row = ['@SQ']
+				if refID in refLens:
+					row.append('LN:'+self.refLens[refID])
+				else:
+					row.append('LN:*')
+				if refID in refURIs:
+					row.append('UR:'+self.refURIs[refID])
+					writer.writerow(row)
+
+			writer.writerow(['@PG', 'ID:EpickiKonwerter', 'VN:0.0.0.0.0.0.0.0.1'])
+
+			ref = '*'
+			start = '*'
+			mapq = '*'
+			cigar = '*'
+			quality = '*'
+			tags = ''
+			for i in xrange(len(self.sequences)):
+				if self.seqIDs: seqID = self.seqIDs[i]
+				else: seqID = 'seq%d' % i
+				if self.references: ref = self.references[i]
+				if self.starts: start = self.starts[i]
+				if self.MAPQ: mapq = self.MAPQ[i]
+				if self.CIGAR: cigar = self.CIGAR[i]
+				if self.quality: quality = self.quality[i]
+				if self.descriptions: tags = self.descriptions[i]
+				writer.writerow([seqID, '*', ref, start, 	mapq, cigar, '*', '*', '*', self.sequences[i], quality, tags])
