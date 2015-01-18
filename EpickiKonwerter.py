062af8d4caafb272a8f6901f021961a206af5a32
ania macioszek <a.macioszek@students.mimuw.edu.pl>
2015-01-18 22:21:54 +0100

    writeFASTA() function

--- EpickiKonwerter.py
+++ EpickiKonwerter.py
@@ -23,7 +23,21 @@ class EpickiKonwerter:
 				self.sequences[-1] += line.strip()
 
 
+	def splitSequence( self, sequence, length=80 ):
+		list_of_sequences = []
+		for i in xrange( len(sequence) / length ):
+			list_of_sequences.append( sequence[ i*length : (i+1)*length ] )
+		if len(sequence) % length != 0:
+			list_of_sequences.append( sequence[ (i+1)*length : ] )
+		return list_of_sequences
+
 
 	def writeFASTA( self, filename ):
-		return 0
+		output = open( filename, 'w' )
+		for i, sequence in enumerate( self.sequences ):
+			output.write( '>' + self.descriptions[i] + '\n' )
+			sequences_to_write = '\n'.join( self.splitSequence(sequence) )
+			output.write( sequences_to_write )
+			output.write('\n')
+
 
