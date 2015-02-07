c7ed4426bb5f3b4ce3607bdbf922e71887c7697f
Marcin Świstak <m.swistak@students.mimuw.edu.pl>
2015-02-07 17:57:29 +0100

    filtering upon convertion using comandline UI

--- convert.py
+++ convert.py
@@ -5,7 +5,7 @@ import argparse
 
 def main():
 
-	parser = argparse.ArgumentParser( description='Convert sequence file formats.' )
+	parser = argparse.ArgumentParser( description='Convert sequence file formats with optional filtering.' )
 	parser.add_argument( '-i', metavar='<inputfile>', type=str, \
 			     required=True, nargs=1, help='input file path' )
 	parser.add_argument( '-o', metavar='<outputfile>', type=str, \
@@ -14,13 +14,16 @@ def main():
 			     required=True, nargs=2, \
 			     choices=['fasta', 'fastq', 'gff2', 'gff3', 'sam'], \
 			     help='input and output file formats' )
+	parser.add_argument( '-e', metavar='<expression>', type=str, \
+			     default=[False], nargs=1, \
+                             help='expression to be evaluated' )
 
 	args = parser.parse_args()
 
-	convertFile( args.i[0], args.o[0], args.f[0], args.f[1] )
+	convertFile( args.i[0], args.o[0], args.f[0], args.f[1], args.e[0] )
 
 	
-def convertFile( inFile, outFile, inFormat, outFormat ):
+def convertFile( inFile, outFile, inFormat, outFormat, expression=False ):
 
 	e = EpickiKonwerter.EpickiKonwerter()
 
@@ -30,6 +33,7 @@ def convertFile( inFile, outFile, inFormat, outFormat ):
 			    [e.writeFASTA, e.writeFASTQ, e.writeGFF2, e.writeGFF3, e.writeSAM] ) )
 
 	reader[ inFormat ]( inFile )
+	if expression:	e.filter( expression )
 	writer[ outFormat ]( outFile )
 
 
